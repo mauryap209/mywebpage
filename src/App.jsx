5bb95cfb21aaa6453f5857aff102ea12fecf60bd
Pankaj Maurya <mauryap209@gmail.com>
2025-06-20 13:01:13 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,14 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <Home 
+          // imgsrc="https://img.freepik.com/premium-photo/illustration-of-school-girl_635702-1714.jpg"
+          // title="My Project"
+          // description="This is a sample React project showcasing various cards with links to different projects."
+          // link="https://mywebreact.netlify.app/"
+          
+         />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,96 @@
 import React from "react";
-
-const Home = () => {
+// import img from "../img/reactweb.png";
+// import img from "../img/education.png";
+const Home = ({ imgsrc, title, description }) => {
   return (
     <div>
-      <h1 className="home">Home</h1>
+      <h1 className="home">{title}</h1>
 
-      <p className="home">
-        Welcome to the home page of our website! Here, you can find various
-        resources and information about our services. Feel free to explore and
-        learn more about what we offer.
-      </p>
+      {/* Card  of Start*/}
+
+
+  <div className="card-container">
+    {/* <div className ="card">
+      <img src={imgsrc} alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">{title}</h3>
+        <p className="card-text">{description}</p>
+         <a className="responsive" href="https://mywebreact.netlify.app/">Click Me</a>
+      </div>
+    </div> */}
+
+
+
+
+
+
+
+
+
+
+    <div className ="card">
+      <img src="https://img.freepik.com/premium-photo/illustration-of-school-girl_635702-1714.jpg" alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">React web page</h3>
+        <p className="card-text">My new react project.</p>
+         <a className="responsive-link" href="https://mywebreact.netlify.app/">Click Me</a>
+      </div>
+    </div>
+
+    <div className ="card">
+      <img src="https://img.freepik.com/premium-photo/illustration-of-school-girl_635702-1714.jpg" alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">Education </h3>
+        <p className="card-text">education for univercity.</p>
+        <a className="responsive-link" href="https://educationtwo.netlify.app/">Click Me</a>
+      </div>
+    </div>
+
+<div className="card">
+      <img src="https://img.freepik.com/premium-photo/illustration-of-school-girl_635702-1714.jpg" alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">My Nature</h3>
+        <p className="card-text">My Nature project description.</p>
+        <a className="responsive-link" href="https://mauryap209.github.io/MyNature/">Click Me</a>
+      </div>
+    </div>
+
+    <div className="card">
+      <img src="https://i.pinimg.com/736x/e4/e9/c4/e4e9c491ee11efe67b691d17360fada7.jpg" alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">Kids zone</h3>
+        <p className="card-text">simple bootstrap kids zone.</p>
+        <a className="responsive-link" href="https://kidingzone.netlify.app/">Click Me</a>
+          
+      </div>
+    </div>
+
+    
+    
+
+    <div className="card">
+      <img src="https://img.freepik.com/premium-photo/illustration-of-school-girl_635702-1714.jpg" alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">Card Title 4</h3>
+        <p className="card-text">This is a sample description for the Four card.</p>
+        <a className="responsive-link" href="https://mywebreact.netlify.app/">Click Me</a>
+      </div>
+    </div>
+
+    <div className="card">
+      <img src="https://img.freepik.com/premium-photo/illustration-of-school-girl_635702-1714.jpg" alt="Card image"/>
+      <div className="card-content">
+        <h3 className="card-title">Card Title 4</h3>
+        <p className="card-text">This is a sample description for the Four card.</p>
+        <a className="responsive-link" href="https://mywebreact.netlify.app/">Click Me</a>
+      </div>
+    </div>
+  </div>
+
+
+      {/* End of Card */}
+
+
     </div>
   );
 };
